373b561580ffc29a031c54861540a19db39308bb
oneloveoneheart <gahetaflorent@gmail.com>
2024-02-13 20:28:01 +0100

    I managed to change display of choosen game

--- script.js
+++ script.js
@@ -13,7 +13,27 @@ function new_game() {
     hide(gameTwoDiv);
 }
 
+function displayGameOne(){
+    let gameOneDiv = document.getElementById("game1");
+    let gameTwoDiv = document.getElementById("game2");
+    show(gameOneDiv);
+    hide(gameTwoDiv);
+}
+
+function displayGameTwo(){
+    let gameOneDiv = document.getElementById("game1");
+    let gameTwoDiv = document.getElementById("game2");
+    show(gameTwoDiv);
+    hide(gameOneDiv);
+}
+
 function main(){
+    let chooseGameOneBtn = document.getElementById("start_game1");
+    chooseGameOneBtn.addEventListener("click", displayGameOne);
+
+    let  chooseGameTwoBtn = document.getElementById("start_game2");
+    chooseGameTwoBtn.addEventListener("click", displayGameTwo);
+
     new_game();
 }
 
