f151ffd1abe7742bd6ef351699641951a2c9400e
oneloveoneheart <gahetaflorent@gmail.com>
2024-02-13 21:32:29 +0100

    I managed to set up the game one and works

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let computerNumber = 0;
 let gameSelection;
 let gameOneDiv;
 let gameTwoDiv;
+let gameOneRestartButton;
 
 document.addEventListener("DOMContentLoaded", () =>{
     gameSelection = document.querySelector("#game_selection");
     gameOneDiv = document.getElementById("game1");
     gameTwoDiv = document.getElementById("game2");
+    gameOneRestartButton = document.getElementById("restart_button1");
 })
 
 function show(element) {
@@ -35,10 +37,28 @@ function displayGameTwo(){
 
 function startPlayerGuessGame(){
     computerNumber = Math.floor(Math.random() * 100) + 1;
-    let restartButton = document.getElementById("restart_button1");
-    restartButton.style.display = "none";
+    gameOneRestartButton.style.display = "none";
     displayGameOne();
     hide(gameSelection);
+}
+
+function treatPlayerInput(){
+    let playerInputElement = document.getElementById("player_input");
+    let entryNumber = parseInt(playerInputElement.value);
+    let messageDiv = document.getElementById("message1");
+
+    if (entryNumber < computerNumber){
+        messageDiv.innerText = "Non c'est plus grand";
+    }
+    else if (entryNumber > computerNumber){
+        messageDiv.innerText = "Non c'est plus petit";
+    }
+    else {
+        messageDiv.innerText = "Bravo, vous avez trouvé " + computerNumber;
+        gameOneRestartButton.style.display = "";
+        playerInputElement.value = "";
+    }
+
 }
 
 function main(){
@@ -47,6 +67,9 @@ function main(){
 
     let  chooseGameTwoBtn = document.getElementById("start_game2");
     chooseGameTwoBtn.addEventListener("click", displayGameTwo);
+
+    let tryButton = document.getElementById("try_button");
+    tryButton.addEventListener("click", treatPlayerInput);
 
     new_game();
 }
